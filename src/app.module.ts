68ab7ced0e70dec5b07c859d364fc7afbad483c5
Rey Gienaldo Jonathan <reygienaldo.j@gmail.com>
2024-08-31 15:53:06 +0700

    Install necessary package and implement other services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
